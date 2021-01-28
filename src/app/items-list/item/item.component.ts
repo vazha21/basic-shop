c6242bc8f1e25c4b15f27a77a902b220077f5770
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-01-28 20:04:52 +0400

    fix: handled unauthorised 'add to card' click

--- src/app/items-list/item/item.component.ts
+++ src/app/items-list/item/item.component.ts
@@ -1,25 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Item} from './item.model';
-import {ItemService} from '../../shared/item.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from './item.model';
+import { ItemService } from '../../shared/item.service';
+import { AuthService } from '../../shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
   @Input() inCart = false;
   @Input() currentItem: Item;
-  constructor(private itemService: ItemService) { }
-  ngOnInit(): void {
-  }
 
-  addItemToCart(item: Item): void{
+  constructor(
+    private itemService: ItemService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  addItemToCart(item: Item): void {
+    if (!this.authService.currentLoggedUser) {
+      alert('Please log in first!');
+      this.router.navigate(['/login']);
+      return;
+    }
     this.itemService.addItemInCart(item);
   }
 
   removeItemFromCart(item: Item): void {
     this.itemService.removeItemFromCart(item);
   }
-
 }
